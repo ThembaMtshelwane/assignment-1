0ac92999cfe63268069f974e4caa515291664897
ThembaMtshelwane <thembamm3@gmail.com>
2024-07-10 15:08:34 +0200

    Page structure components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import About from './Components/About/About'
+import Article from './Components/Article/Article'
+import Subscribe from './Components/Subscribe/Subscribe'
 
 function App() {
   return (
     <>
+    <Navbar/>
+    <Hero/>
+    <Article/>
+    <About/>
+    <Subscribe/>      
     </>
   )
 }
