330c8e5208fe6da8268618bfa62cccfbedbc0662
Themba Mtshelwane <Thembamm3@gmail.com>
2024-07-10 19:44:49 +0200

    logo component

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,24 @@
 import AboutLink from '../Links/AboutLink'
 import ArticleLink from '../Links/ArticleLink'
 // import styles from './navbar.module.css'
-
+import logoStyles from './logo.module.css'
 
 function Navbar() {
   return (
     <nav>
       <ul>
         <li>
-          <span className={styles.logoLetterD}>D</span>
-          <span className={styles.logoLetterZ}>Z</span>
-        </li >
-        
-        <li><AboutLink name='About'/></li>
-        <li><ArticleLink/></li>
-             
+          <span className={logoStyles.logoLetterD}>D</span>
+          <span className={logoStyles.logoLetterZ}>Z</span>
+        </li>
+
+        <li>
+          <AboutLink name="About" />
+        </li>
+        <li>
+          <ArticleLink />
+        </li>
+
         <li>Subscribe</li>
       </ul>
     </nav>
