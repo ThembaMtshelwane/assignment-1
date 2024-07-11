5f0c2be71d6d19a4af26ec5a70a53ba5455f7d55
ThembaMtshelwane <thembamm3@gmail.com>
2024-07-11 09:17:01 +0200

    hero component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import './hero.css'
 function Hero() {
   return (
-    <div>Hero</div>
+    <section className='heroContainer'>
+    <h1>DRONOTICZ</h1>
+    <h3>The Era of Drones</h3>
+    <span className='bar'></span>
+    </section>
   )
 }
 
