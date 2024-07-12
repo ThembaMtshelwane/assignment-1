ff976a8ee698a10b7ef0de76bb616af041c962e4
ThembaMtshelwane <thembamm3@gmail.com>
2024-07-12 11:42:18 +0200

    articles section: removing borders

--- src/Components/ArticlesSection/ArticlesSection.jsx
+++ src/Components/ArticlesSection/ArticlesSection.jsx
@@ -1,55 +1,53 @@
-import React from 'react'
-import Article from './Article/Article'
-import SectionHeader from '../SectionHeader/SectionHeader'
-import {articleInformation} from '../../data'
+import Article from "./Article/Article";
+import SectionHeader from "../SectionHeader/SectionHeader";
+import { articleInformation } from "../../data";
 
-import './articlesSection.css'
+import "./articlesSection.css";
 function ArticleSection() {
   return (
-    <section className='articles-section' id='articles'>
-        <SectionHeader name='LATEST ARTICLES'style='sub-section-bar' />
-        <ul className='articles-list'>
-           {/* {articleInformation.map(article => {
+    <section className="articles-section" id="articles">
+      <SectionHeader name="LATEST ARTICLES" style="sub-section-bar" />
+      <ul className="articles-list">
+        {/* {articleInformation.map(article => {
                   <li>
                     <Article imageURL={article.imageURL} imageName={article.imageName} title={article.title} />
                   </li>
                   // console.log(article.imageName)
            })}  */}
-           <li>
-             <Article 
+        <li>
+          <Article
             imageURL={articleInformation[0].imageURL}
             imageName={articleInformation[0].imageName}
-            title={articleInformation[0].title}/>
-           </li>
+            title={articleInformation[0].title}
+          />
+        </li>
 
-           <li>
-             <Article 
+        <li>
+          <Article
             imageURL={articleInformation[1].imageURL}
             imageName={articleInformation[1].imageName}
-            title={articleInformation[1].title}/>
-           </li>
+            title={articleInformation[1].title}
+          />
+        </li>
 
-           <li>
-             <Article 
+        <li>
+          <Article
             imageURL={articleInformation[2].imageURL}
             imageName={articleInformation[2].imageName}
-            title={articleInformation[2].title}/>
-           </li>
+            title={articleInformation[2].title}
+          />
+        </li>
 
-           <li>
-             <Article 
+        <li>
+          <Article
             imageURL={articleInformation[3].imageURL}
             imageName={articleInformation[3].imageName}
-            title={articleInformation[3].title}/>
-           </li>
-           
-
-
-          
-        </ul>
-     
+            title={articleInformation[3].title}
+          />
+        </li>
+      </ul>
     </section>
-  )
+  );
 }
 
-export default ArticleSection
+export default ArticleSection;
