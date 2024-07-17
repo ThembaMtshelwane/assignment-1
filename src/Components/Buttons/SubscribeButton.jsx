c36dfb947f6646fda7c7f053c04163c03e1db208
ThembaMtshelwane <thembamm3@gmail.com>
2024-07-17 15:24:03 +0200

    Buttons hover state, additional font backups amd subscribe section posit

--- src/Components/Buttons/SubscribeButton.jsx
+++ src/Components/Buttons/SubscribeButton.jsx
@@ -1,10 +1,11 @@
 import Button from "./Button";
+import "./subscribeButton.css";
 
 function SubscribeButton() {
   return (
     <>
       <a href="#subscribe">
-        <Button name="subscribe" />
+        <Button name="subscribe" styles="subscribe-button" />
       </a>
     </>
   );
