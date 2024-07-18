29210335a23ff5db23bfeb563536cce728bd4540
ThembaMtshelwane <thembamm3@gmail.com>
2024-07-18 14:12:46 +0200

    Responsive for 619px and lower: Addded a responsive navbar with a sidebar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,11 +3,18 @@ import ArticleLink from "../Links/ArticleLink";
 import SubscribeButton from "../Buttons/SubscribeButton";
 import "./navbar.css";
 import logoStyles from "./logo.module.css";
+import { useState } from "react";
+import { IoMdMenu } from "react-icons/io";
 
 function Navbar() {
+  const [toggleNav, setToggle] = useState(false);
+  const handleClick = () => {
+    setToggle(!toggleNav);
+  };
+
   return (
     <nav>
-      <ul>
+      <ul className="desktop-navbar">
         <li>
           <a href="#hero">
             <span className={logoStyles.logoLetterD}>D</span>
@@ -24,6 +31,38 @@ function Navbar() {
           <SubscribeButton />
         </li>
       </ul>
+      <ul className="hamburuger-nav">
+        <li>
+          <a href="#hero">
+            <span className={logoStyles.logoLetterD}>D</span>
+            <span className={logoStyles.logoLetterZ}>Z</span>
+          </a>
+        </li>
+        <li className="hamburger-icon" onClick={handleClick}>
+          <IoMdMenu />
+        </li>
+      </ul>
+      {toggleNav ? (
+        <section className="sidebar">
+          <ul className="mobile-navbar">
+            <li className="close-icon" onClick={handleClick}>
+              X
+            </li>
+            <li>
+              <AboutLink name="About" />
+            </li>
+            <li>
+              <ArticleLink />
+            </li>
+
+            <li>
+              <SubscribeButton />
+            </li>
+          </ul>
+        </section>
+      ) : (
+        ""
+      )}
     </nav>
   );
 }
