d2d5eaa7fb6a6cbb23a10a2099d2bfbb8390e351
ThembaMtshelwane <thembamm3@gmail.com>
2024-09-09 14:45:12 +0200

    Subscribe section

--- src/Components/SectionHeader/SectionHeader.jsx
+++ src/Components/SectionHeader/SectionHeader.jsx
@@ -1,8 +1,8 @@
-function SectionHeader({ name }) {
+function SectionHeader({ name, style = "" }) {
   return (
     <section className="text-2xl w-fit my-2 sm:text-4xl border-2 flex flex-col md:mt-6">
-      <h3>{name}</h3>
-      <span className="mx-auto w-[35%] my-8 bg-black h-full border-2 border-black"></span>
+      <h3 className="uppercase">{name}</h3>
+      <span className={`mx-auto w-[35%] my-8 h-full border-2 ${style}`}></span>
     </section>
   );
 }
--- src/Components/SubscribeSection/SubscribeSection.jsx
+++ src/Components/SubscribeSection/SubscribeSection.jsx
@@ -1,19 +1,32 @@
 import SectionHeader from "../SectionHeader/SectionHeader";
 
-
 function SubscribeSection() {
   const handleSubmit = (event) => {
     event.preventDefault();
   };
   return (
-    <section className="subscribe-section" id="subscribe">
-      <SectionHeader name="Subscribe" style="sub-section-bar-none" />
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p className="call-to-action"> Sign-up to our newsletter</p>
-          <input type="email" id="subscribe-input" placeholder="Your email" />
+    <section
+      className="h-[50vh] max-h-[500px] flex flex-col items-center pt-[100px] min-h-[400px] border-2"
+      id="subscribe"
+    >
+      <SectionHeader name="Subscribe" style="hidden" />
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col w-[80%] max-h-[250px] max-w-[450px] items-center mt-[15px] md:w-[85%] border-2 border-black"
+      >
+        <label className="flex flex-col items-center w-full mb-[45px]">
+          <p className="mb-[25px] font-semibold"> Sign-up to our newsletter</p>
+          <input
+            className="w-full p-[20px] border-none"
+            type="email"
+            id="subscribe-input"
+            placeholder="Your email"
+          />
         </label>
-        <button type="submit" className="submit-buttton">
+        <button
+          type="submit"
+          className="uppercase px-8 py-3 border-2 border-black"
+        >
           Submit
         </button>
       </form>
