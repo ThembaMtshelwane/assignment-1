96e53a56a3cf8a524977db2a31d7e1c9cba57946
ThembaMtshelwane <thembamm3@gmail.com>
2024-09-10 09:08:22 +0200

    Footer

--- src/Components/SectionHeader/SectionHeader.jsx
+++ src/Components/SectionHeader/SectionHeader.jsx
@@ -2,7 +2,9 @@ function SectionHeader({ name, style = "" }) {
   return (
     <section className="text-2xl w-fit my-2 sm:text-4xl border-2 flex flex-col md:mt-6">
       <h3 className="uppercase">{name}</h3>
-      <span className={`mx-auto w-[35%] my-8 h-full border-2 ${style}`}></span>
+      <span
+        className={`mx-auto w-[35%] my-8 border-2 border-black ${style}`}
+      ></span>
     </section>
   );
 }
