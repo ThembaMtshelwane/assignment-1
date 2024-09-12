69d7db4ff5b460ff562da762a1fda87001eda8fb
ThembaMtshelwane <thembamm3@gmail.com>
2024-09-12 09:39:30 +0200

    Minor fixes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
         backgroundPosition: "center",
       }}
     >
-      <h1 className="text-5xl xsm:text-6xl sm:text-7xl md:text-[112px] mb-10">
+      <h1 className="text-4xl xsm:text-6xl sm:text-7xl md:text-[112px] mb-10">
         DRONOTICZ
       </h1>
       <SectionHeader name="The Era of Drones" />
--- src/Components/SectionHeader/SectionHeader.jsx
+++ src/Components/SectionHeader/SectionHeader.jsx
@@ -1,7 +1,7 @@
 function SectionHeader({ name, style = "" }) {
   return (
     <section className="text-2xl w-fit sm:text-4xl flex flex-col md:mt-6">
-      <h3 className="uppercase mb-5 tracking-wider">{name}</h3>
+      <h3 className="uppercase mb-5 tracking-wider  text-center">{name}</h3>
       <span
         className={`mx-auto w-[100px] h-[2px] bg-black mt-5 border-black ${style}`}
       ></span>
